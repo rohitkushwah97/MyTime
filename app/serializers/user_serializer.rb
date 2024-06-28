367ffeb8860c192cd02090694fb9f17bc8e9911b
Rohti <rohitkushwah6@gmail.com>
2024-06-28 13:30:54 +0530

    user serializer changes

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :phone_number, :full_name, :about_us, :profile_image, :devices, :address, :contact_status, :notification_status, :posts, :created_at, :updated_at
+  attributes :id, :email, :phone_number, :full_name, :about_as, :profile_image, :devices, :address, :contact_status, :notification_status, :posts, :created_at, :updated_at
 
   def profile_image
     return unless object.profile_image.attached?
